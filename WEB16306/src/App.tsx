0ff08bd0e0cc846433745a9eabd3cc5da19d6a93
tuanndph16237 <nguyenductuan05102002@example.com>
2022-03-22 12:51:45 +0700

    init

--- WEB16306/src/App.tsx
+++ WEB16306/src/App.tsx
@@ -1,32 +1,66 @@
 import { useEffect, useState } from 'react'
+import { Routes, Route, Navlink, Navigate } from 'react-router-dom';
 import logo from './logo.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./dashboard.css";
+
 import ShowInfo from './components/ShowInfo'
 import Product from './components/Product'
-import { list } from './api/products'
+import { add, list, remove } from './api/products';
+import axios from 'axios';
+import type { IProduct } from './types/product';
+import AdminLayout from './pages/Layout/adminLayout';
+import WebsiteLayout from './pages/Layout/WebsiteLayout';
+import DashBoard from './pages/dashboard';
+import ProductManager from './pages/productManager';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import ProductAdd from './pages/ProductAdd';
 
-interface IProduct{
-  id: number, 
-  name: string
-}
-function App() {
+function App(){
+
   const [count, setCount] = useState(0)
-const [products, setProducts]= useState<IProduct[]>([]);
+  const [products, setProducts] = useState<IProduct[]>([]);
+  useEffect(() =>{
+    const getProducts = async() => {
+      const { data } =await list();
+      setProducts(data);
+    };
+    getProducts();
+  }, [])
+  const removeItem = (id: number) =>{
 
-useEffect(()=>{
-  const getProducts = async ()=>{
-    const response = await fetch('http://localhost:3001/api/products');
-    const data = await response.json();
-    setProducts(data);
-  };
-  getProducts();
-}, [])
-  return (
-    <div className="App">
-      <ShowInfo name="abc" age={10}/>
-      {products.map(item => <div>{item.name}</div>)}
-    </div>
-  )
+  }
+  const onHandleAdd = async (product: IProduct)=>{
+    const {data} = await add(product);
+    setProducts([...products, data]);
+  }
+  <div className='App'>
+    <header>
+      <ul>
+        <li>
+          <Navlink to="/">Home Page</Navlink>
+        </li>
+        <li>
+          <Navlink to="/product">Product Page</Navlink>
+        </li>
+        <li>
+          <Navlink to="/about">About</Navlink>
+        </li>
+      </ul>
+    </header>
+    <main>
+      <Routes>
+        <Route path='/' element={WebsiteLayout />}>
+          <Route index element={<Home />}>
+            <Route path='product'>
+              <Route index element={<h1>Hien thi san Pham</h1>}/>
+              <Route path=':id' element={}/>
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </main>
+  </div>
 }
-
 export default App
