9404a6d956c9713e66e4ef62679f052894ee03c5
tuanndph16237 <nguyenductuan05102002@example.com>
2022-03-24 16:14:26 +0700

    remove &change

--- WEB16306/src/App.tsx
+++ WEB16306/src/App.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import ShowInfo from './components/ShowInfo'
-import Product from './components/Product'
-import { list } from './api/products'
+
 
 interface IProduct{
   id: number, 
@@ -11,20 +9,11 @@ interface IProduct{
 }
 function App() {
   const [count, setCount] = useState(0)
-const [products, setProducts]= useState<IProduct[]>([]);
 
-useEffect(()=>{
-  const getProducts = async ()=>{
-    const response = await fetch('http://localhost:3001/api/products');
-    const data = await response.json();
-    setProducts(data);
-  };
-  getProducts();
-}, [])
+
   return (
     <div className="App">
-      <ShowInfo name="abc" age={10}/>
-      {products.map(item => <div>{item.name}</div>)}
+      
     </div>
   )
 }
--- WEB16306/src/types/product.tsx
+++
@@ -1,4 +0,0 @@
-export interface IProduct{
-    _id:number,
-    name: string
-}
