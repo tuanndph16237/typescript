ef77564f3a25dc3d83ed182b7150a66b883fa98f
tuanndph16237 <71483286+tuanndph16237@users.noreply.github.com>
2022-04-11 21:08:54 +0700

    cat dat them moi truong

---
+++ WEB16306/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{html,tsx,js}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    prefix: "tw-",
+}
